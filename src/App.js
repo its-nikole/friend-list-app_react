c1bc49f2327c743e09dcec8cbccb077ac38d1114
Nikole <105383998+its-nikole@users.noreply.github.com>
2022-10-19 20:49:57 +0300

    simple form

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { AddFriendForm } from "./addFriendForm";
+
+
+const friends = [
+    { firstName: "John", lastName: "Smith", age: 32, city: "Kaunas" },
+    { firstName: "Maria", lastName: "Hudghes", age: 28, city: "Siauliai" },
+    { firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
+  ];
+  
+export function App() {
+    return (
+        <>
+        <AddFriendForm />
+        
+        
+        </>
+    )
+}
