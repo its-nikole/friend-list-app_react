a9bb7ad3a3568a6c7694104b02b638a9858639bb
Nikole <105383998+its-nikole@users.noreply.github.com>
2022-10-26 21:44:00 +0300

    Ties submit events - #28

--- src/App.js
+++ src/App.js
@@ -7,29 +7,45 @@ const friends = [
   { firstName: "Thomas", lastName: "Muiller", age: 33, city: "Vilnius" },
 ];
 
+const initialFormData = { firstName: "", lastName: "", age: "", city: "" };
+
 const handleSubmit = (e) => {
   e.preventDefault();
   alert("Friend added to the list!");
 };
 
 export function App() {
+  const [formState, setFormState] = useState(initialFormData);
+
+  const [friendsState, setFriendsState] = useState(friends);
+
   return (
     <>
-      <div class="wrapper">
+      <div className="wrapper">
         <form onSubmit={handleSubmit}>
           <fieldset>
             <legend>Add New Friend</legend>
-            <input placeholder="First Name" />
-            <input placeholder="Last Name" />
-            <input placeholder="Age" />
-            <input placeholder="City" />
+            <input
+              id="firstName"
+              value={formState.firstName}
+              placeholder="First Name"
+              onChange={(e) => setFormState(e.target.value, e.target.id)}
+            />
+            <input
+              id="lastName"
+              value={formState.lastName}
+              placeholder="Last Name"
+              onChange={(e) => setFormState(e.target.value, e.target.id)}
+            />
+            <input id="age" value={formState.age} placeholder="Age" onChange={(e) => setFormState(e.target.value, e.target.id)}/>
+            <input id="city" value={formState.city} placeholder="City" onChange={(e) => setFormState(e.target.value, e.target.id)}/>
             <button>Add Friend</button>
           </fieldset>
         </form>
       </div>
-      <div class="friends-container">
-        {friends.map((friend) => (
-          <div className="FriendCard">
+      <div className="friends-container">
+        {friendsState.map((friend) => (
+          <div className="FriendCard" key={friend.firstName}>
             <p>First Name: {friend.firstName}</p>
             <p>Last Name: {friend.lastName}</p>
             <p>Age: {friend.age}</p>
